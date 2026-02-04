23fdb62f590d950a6170a68e930903cdf9421f4f
Wolfiito <raflores2310@gmail.com>
2026-02-03 20:03:02 -0600

    Imagenes

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -10,12 +10,12 @@ interface TimelineProps {
 }
 const DUSTY_PINK_TEXT = "text-[#DB8C8A]";
 const IMAGE_MAP: Record<string, { inactive: string; active: string }> = {
-  Recepcion: { inactive: "/images/recepcion_1", active: "/images/recepcion_2.png" },
+  Recepcion: { inactive: "/images/recepcion_1.png", active: "/images/recepcion_2.png" },
   Ceremonia: { inactive: "/images/ceremonia_2.png", active: "/images/ceremonia_1.png" },
   Fotos: { inactive: "/images/fotos_2.png", active: "/images/fotos_1.png" },
   Comida: { inactive: "/images/Banquete_2.png", active: "/images/Banquete_1.png" },
   Brindis: { inactive: "/images/Fiesta_2.png", active: "/images/Fiesta_1.png" },
-  Salida: { inactive: "/images/iconos_Despedida1", active: "/images/iconos_Despedida2"}
+  Salida: { inactive: "/images/iconos_Despedida1.png", active: "/images/iconos_Despedida2.png"}
 };
 
 const textVariants: Variants = {
